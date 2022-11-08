85eb016b3595316617737532c794ce6396023430
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-08 14:50:15 +0100

    Document.GetElementById

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ let cognome = prompt("Inserisci cognome");
 let colore = prompt("Inserisci colore preferito");
 
 console.log(nome + cognome + colore + "21");
+
+document.getElementById("tuonome").innerHTML = nome;
+document.getElementById("tuocognome").innerHTML = cognome;
+document.getElementById("tuocolore").innerHTML = colore;
+document.getElementById("tuapassword").innerHTML = console.log;
