6564cb7903f16ac94d76006d468e3f79dc69243d
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-08 15:36:54 +0100

    variabili numeri

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 let nome = prompt("Inserisci nome");
 let cognome = prompt("Inserisci cognome");
 let colore = prompt("Inserisci colore preferito");
+let numero1 = prompt("Inserisci primo numero");
+let numero2 = prompt("Inserisci secondo numero");
 
-console.log(nome + cognome + colore + "21");
+/* console.log(nome + cognome + colore + "21"); */
 
 document.getElementById("tuonome").innerHTML = nome;
 document.getElementById("tuocognome").innerHTML = cognome;
