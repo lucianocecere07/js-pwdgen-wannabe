bc86c8a48189e279ba7ff9e5d8c12989e713ce63
lucianocecere07 <cecereluciano97@gmail.com>
2022-11-08 15:46:12 +0100

    parseint dei numeri e divisione dei numeri

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ let numero2 = prompt("Inserisci secondo numero");
 
 /* console.log(nome + cognome + colore + "21"); */
 
+numero1 = parseInt(numero1);
+numero2 = parseInt(numero2);
+divisione = numero1 / numero2;
+
+console.log(nome + cognome + colore + divisione);
+
 document.getElementById("tuonome").innerHTML = nome;
 document.getElementById("tuocognome").innerHTML = cognome;
 document.getElementById("tuocolore").innerHTML = colore;
